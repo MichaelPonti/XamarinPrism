efc04f5178a4dcf9912a02ece40bece6accab0ea
Michael Ponti <michael.ponti@live.ca>
2016-10-26 21:09:53 -0700

    Added a solution that shows the app object in action.

---
+++ DevDaySpeakers01/DevDaysSpeakers/DevDaysSpeakers.Shared/App.cs
@@ -0,0 +1,65 @@
+﻿using DevDaysSpeakers.View;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DevDaysSpeakers
+{
+    //public class App : Application
+    //{
+    //    public App()
+    //    {
+    //        // The root page of your application
+    //        var content = new SpeakersPage();
+
+    //        MainPage = new NavigationPage(content);
+    //    }
+
+    //    protected override void OnStart()
+    //    {
+    //        // Handle when your app starts
+    //    }
+
+    //    protected override void OnSleep()
+    //    {
+    //        // Handle when your app sleeps
+    //    }
+
+    //    protected override void OnResume()
+    //    {
+    //        // Handle when your app resumes
+    //    }
+    //}
+
+	public class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null)
+			: base(initializer)
+		{
+		}
+
+		protected override void OnInitialized()
+		{
+			NavigationService.NavigateAsync("SpeakersPage");
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<View.SpeakersPage>();
+		}
+
+		protected override void OnSleep()
+		{
+			base.OnSleep();
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+		}
+	}
+}
---
+++ DevDaySpeakers01/DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using DevDaysSpeakers.Model;
+using DevDaysSpeakers.Services;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace DevDaysSpeakers.ViewModel
+{
+	public class SpeakersViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged([CallerMemberName] string name = null) =>
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+
+		private bool _isBusy = false;
+		public bool IsBusy
+		{
+			get { return _isBusy; }
+			set
+			{
+				_isBusy = value;
+				OnPropertyChanged();
+				GetSpeakersCommand.ChangeCanExecute();
+			}
+		}
+
+
+		public ObservableCollection<Speaker> Speakers { get; set; } = 
+			new ObservableCollection<Speaker>();
+
+
+		private async Task GetSpeakers()
+		{
+			if (IsBusy)
+				return;
+
+			Exception error = null;
+			try
+			{
+				IsBusy = true;
+
+				using (var client = new HttpClient())
+				{
+					//grab json from server
+					var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+
+					//Deserialize json
+					var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
+
+					//Load speakers into list
+					Speakers.Clear();
+					foreach (var item in items)
+						Speakers.Add(item);
+				}
+
+			}
+			catch (Exception ex)
+			{
+				error = ex;
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+
+			if (error != null)
+				await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
+		}
+
+
+		private Command _getSpeakersCommand = null;
+		public Command GetSpeakersCommand
+		{
+			get
+			{
+				return _getSpeakersCommand ??
+					(_getSpeakersCommand = new Command(
+						async () => await GetSpeakers(), 
+						() => !IsBusy));
+			}
+		}
+	}
+}
