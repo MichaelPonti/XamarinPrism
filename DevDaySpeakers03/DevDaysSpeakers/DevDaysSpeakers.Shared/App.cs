78b62cca7c1679f740b523dc1dc52e809c4c05d4
Michael Ponti <michael.ponti@live.ca>
2016-10-28 19:29:48 -0700

    Added step03 from copying 02

---
+++ DevDaySpeakers03/DevDaysSpeakers/DevDaysSpeakers.Shared/App.cs
@@ -0,0 +1,39 @@
+﻿using DevDaysSpeakers.View;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DevDaysSpeakers
+{
+	public class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null)
+			: base(initializer)
+		{
+		}
+
+		protected override void OnInitialized()
+		{
+			NavigationService.NavigateAsync("SpeakersPage");
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<View.SpeakersPage, ViewModel.SpeakersViewModel>();
+		}
+
+		protected override void OnSleep()
+		{
+			base.OnSleep();
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+		}
+	}
+}
